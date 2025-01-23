4e2fc0c4ff15c97622358023e82c7c9ffb3abc4b
Pedro <pepe040906@gmail.com>
2025-01-23 19:58:41 -0300

    feat: configurar database

---
+++ src/test/java/com/gaming/store/gaming_store_users/GamingStoreUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaming.store.gaming_store_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingStoreUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
